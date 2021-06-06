86536629d61c18421a0898318da152894bd386a1
meandhow <84931731+meandhow@users.noreply.github.com>
2021-06-07 00:20:41 +0100

    Add files via upload

---
+++ LDA_LR/positionEstimator.m
@@ -0,0 +1,82 @@
+%%% Team Members:Hugh Dickens, Giorgio Martinelli, Rahel Ohlendorf, Michal
+%%% Olak
+%%% BMI Spring 2021 (Update 17th March 2015)
+
+function [x, y, newModelParameters] = positionEstimator(past_current_trial, modelParameters)
+window_size = 20; %window size used for extraction of position from veloity.
+%change according to the window size used for binning
+
+spike_train_test= past_current_trial.spikes;%Loading the spike train given for testing
+
+
+[neurons,length] = size(spike_train_test); %let's check the dimensions of what we work with
+%the training data used bins. Therefore we need to bin the test data too
+spike_train_binned_test = zeros(100000, neurons); %creating placeholder for the binned spike train.
+spike_angle = zeros(neurons,1);
+
+if length <= 320
+    % find number of spikes
+    for n = 1:neurons
+        spikesnr = sum(spike_train_test(n,1:320));
+        spike_angle(n) = spikesnr;
+    end
+
+% find most predicted direcion and set it as hand direction/reaching angle
+    j = predict(modelParameters.mdl,spike_angle');
+    
+ else
+     % use value that was found prevously
+     j = modelParameters.direction;
+end
+    counter = 1;%counter is for accesing a bin of interest
+    for t = 300:window_size:length-window_size %looping over data from a single neuron.
+        
+        spike_train_binned_test(counter,:) = var(spike_train_test(:,(1+t):(window_size+t)),1,2);
+        %this is where we calculate the value of bin.
+        %binning is basically looking at spike data using
+        %sliding,non-overlaping window. We calculate how many spikes there
+        %are in a given window and take mean of that
+        counter=counter+1;
+    end
+spike_train_binned_test(counter-1:end,:) = [];
+%importing the linear regression model parameters
+x_lr = modelParameters.model1(j).xlr;
+y_lr = modelParameters.model2(j).ylr;
+
+%let's predict the velocities!
+% [x_vels_lr, ~] = predict(x_lr,spike_train_binned_test); 
+% [y_vels_lr, ~] = predict(y_lr,spike_train_binned_test);
+x_vels_lr=spike_train_binned_test*x_lr;
+y_vels_lr=spike_train_binned_test*y_lr;
+dx=sum(x_vels_lr,1);
+dy=sum(y_vels_lr,1);
+%okay, we've got velocities. Now it's time to extract positions from them
+  x = past_current_trial.startHandPos(1) + dx;
+  y = past_current_trial.startHandPos(2) + dy;
+  newModelParameters.model1 = modelParameters.model1;
+  newModelParameters.model2 = modelParameters.model2;
+  %newModelParameters.knn=modelParameters.knn;
+  newModelParameters.mdl=modelParameters.mdl;
+  newModelParameters.direction = j;
+%   if length <= 320
+% 
+%     x = past_current_trial.startHandPos(1);
+% 
+%     y = past_current_trial.startHandPos(2);
+% 
+ if length <= 530
+
+    x = past_current_trial.startHandPos(1) + dx;
+
+    y = past_current_trial.startHandPos(2) + dy;
+
+else
+
+    x = past_current_trial.decodedHandPos(1,end);
+
+    y = past_current_trial.decodedHandPos(2,end);
+ end
+
+
+
+end
---
+++ LDA_LR/positionEstimatorTraining.m
@@ -0,0 +1,82 @@
+%%% Team Members:Hugh Dickens, Giorgio Martinelli, Rahel Ohlendorf, Michal
+%%% Olak
+%%% BMI Spring 2021 
+
+
+function [modelParameters] = positionEstimatorTraining(training_data)
+window_size = 20 ;
+  
+% Calc of velocity from position data. Change window size to reflect the length of interest
+[x_vel_points, y_vel_points,spike_train_binned]= getvel(training_data, window_size) ;
+[trials,angle]=size(training_data);
+neurons=length(training_data(1,1).spikes(:,1));
+
+
+
+%% Linear regression. Y = vel, X = binned 
+%x_lr=zeros(98,8);
+%y_lr=zeros(98,8);
+for j=1:angle
+x_lr =lsqminnorm(spike_train_binned(:,:,j), x_vel_points(:,j)); %getting coefficients
+y_lr = lsqminnorm(spike_train_binned(:,:,j), y_vel_points(:,j));
+% x_lr =polyfit(spike_train_binned(:,:,j), x_vel_points(:,j),2); %getting coefficients
+% y_lr = polyfit(spike_train_binned(:,:,j), y_vel_points(:,j),2);
+model1(j).xlr=x_lr;
+model2(j).ylr=y_lr;
+end
+modelParameters.model1 = model1;
+modelParameters.model2 = model2;
+ %% LDA Classifier
+    % used to predict the reaching angle from the first 340ms
+
+    spikes = [];
+    direction = [];
+    spike_angle = zeros(trials,neurons);
+
+    for a = 1:angle
+        for n = 1:neurons
+            for t = 1:trials
+                    spikesnr = sum(training_data(t,a).spikes(n,1:340));
+                    spike_angle(t,n) = spikesnr;
+            end
+        end
+        %spike_mean=mean(spike_angle,1);
+        spikes = [spikes; spike_angle];
+        angles(1:trials) = a;
+        direction = [direction, angles];
+        %direction=[1:1:8];
+    end
+      %size(spikes)
+      %size(direction)
+    %knn = fitcknn(spikes,direction);
+    mdl=fitcdiscr(spikes,direction,'DiscrimType', 'linear','Delta',0.00028308,'Gamma',0.24285);%,'Prior','empirical');
+    %modelParameters.knn=knn;
+    modelParameters.mdl=mdl;
+end
+
+function [x_vel_points, y_vel_points, spike_train_binned]= getvel(TrainingData, window_size)
+    
+    [trial,angle] = size(TrainingData);
+     x_vel_points = zeros(40000, 1,angle);
+     y_vel_points = zeros(40000, 1,angle);
+     neurons=length(TrainingData(1,1).spikes(:,1));
+     spike_train_binned = zeros(40000,neurons,angle);
+     counter2=1;
+    for i= 1:trial
+        for j=1:angle
+            timesteps=length(TrainingData(i,j).handPos(1,:));
+        for t = 320:window_size:timesteps-window_size
+            valx(:,j)=TrainingData(i,j).handPos(1,window_size+t)-TrainingData(i,j).handPos(1,1+t);
+            x_vel_points(counter2,j) = valx(:,j);
+            valy(:,j)=TrainingData(i,j).handPos(2,window_size+t)-TrainingData(i,j).handPos(2,1+t);
+            y_vel_points(counter2,j) = valy(:,j);
+            val(1,:,j)= var(TrainingData(i,j).spikes(:,(1+t):(window_size+t)),1,2);
+            spike_train_binned(counter2,:,j) = val(1,:,j);
+            counter2 = counter2 +1;
+        end
+        end
+    end
+    x_vel_points(counter2-1:end,:)=[];
+    y_vel_points(counter2-1:end,:)=[];
+    spike_train_binned(counter2-1:end,:,:)=[];
+end
